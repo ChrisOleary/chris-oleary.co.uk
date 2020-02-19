28a93801c7ff3af2f3b4f8efaa37a43dfa4f04e4
Chris O'leary <chris_oleary@hotmail.co.uk>
2020-02-19 07:25:11 +0000

    updated nugets and new mosh controller

---
+++ SpotifyFutureAlbums/Controllers/MoshIntermediateController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SpotifyFutureAlbums.Controllers
+{
+    public class MoshIntermediateController : Controller
+    {
+
+        public class Person
+        {
+            public string Name;
+
+            public void Introduce(string to)
+            {
+                Console.WriteLine("Hi, {0}, my name is {1}", to, Name);
+            }
+        }
+
+        // GET: MoshIntermediate
+        public ActionResult Index()
+        {
+            var person = new Person();
+            person.Name = "Yo Momma";
+            person.Introduce("Chris");
+
+
+
+            return View();
+        }
+    }
+}
