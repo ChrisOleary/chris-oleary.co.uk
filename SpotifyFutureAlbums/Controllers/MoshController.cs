bb5e053ac65d6b493373d6b3dbce49bfa3d3a59a
Chris O'leary <chris_oleary@hotmail.co.uk>
2020-04-07 11:38:59 +0100

    github ui testing

---
+++ SpotifyFutureAlbums/Controllers/MoshController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SpotifyFutureAlbums.Controllers
+{
+    public class MoshController : Controller
+    {
+        // GET: Mosh
+        public ActionResult Index()
+        {
+            var calculator = new Calculator();
+            try
+            {
+                calculator.Add(null);
+                Console.WriteLine($"The calculators answer is : {calculator}");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine($"The input is {calculator}");
+            }
+
+            return View(calculator);
+        }
+
+       public class Calculator
+        {
+            public int Add(params int[] numbers)
+            {
+                int sum = 0;
+                foreach (var tits in numbers)
+                {
+                    sum += tits;
+                }
+                return sum;
+            }
+        }
+
+    }
+}
